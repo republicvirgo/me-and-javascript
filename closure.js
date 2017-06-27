f73d7cef55aa2f4466219d7a513a30da11d32174
sigidhanafi <sigidhanafi@gmail.com>
2017-06-27 12:17:17 +0700

    learn closure

---
+++ closure.js
@@ -0,0 +1,34 @@
+// function greeting () {
+//   const name = 'Sigit Hanafi'
+//   function greetingPeople () {
+//     console.log('Halo, ', name)
+//   }
+//   greetingPeople()
+// }
+
+// greeting()
+
+// function greeting () {
+//   let name = 'Sigit Hanafi'
+//   function greetingPeople () {
+//     console.log('Halo, ', name)
+//   }
+//   function setName (newName) {
+//     name = newName
+//   }
+//   greetingPeople()
+//   setName('Sigit')
+//   greetingPeople()
+// }
+
+// greeting()
+
+// function greeting () {
+//   return function greetingPeople (name) {
+//     console.log('Halo,', name)
+//   }
+// }
+
+// const greet = greeting()
+// greet('Sigit')
+// greet('Hanafi')
